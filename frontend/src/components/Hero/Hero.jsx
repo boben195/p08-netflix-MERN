826e8925d696c54dba2f866eafd2bb956b3e9b6e
boben195 <boben195@ukr.net>
2025-07-23 14:47:38 +0300

    add random bg-image for hero

--- frontend/src/components/Hero/Hero.jsx
+++ frontend/src/components/Hero/Hero.jsx
@@ -1,12 +1,43 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import HeroBg from "../../assets/herobg2.jpg";
 import { Bookmark, Play } from "lucide-react";
 
 const Hero = () => {
+  const [movie, setMovie] = useState(null);
+
+  const options = {
+    method: "GET",
+    headers: {
+      accept: "application/json",
+      Authorization:
+        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyYjQ1NzM2MjhiNTMyMGI5ZDg4OTdhMDU1NGIxZTJkZSIsIm5iZiI6MTcxMjIyNzMwMS43MjcsInN1YiI6IjY2MGU4M2U1MzNhMzc2MDE2NDgyNmQ5MCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.kcBS_BDH-IwwDAmiuT3hifFsiLsDHrnfNF2t-q4kIFw",
+    },
+  };
+  useEffect(() => {
+    fetch(
+      "https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=1",
+      options
+    )
+      .then((res) => res.json())
+      .then((res) => {
+        if (res.results && res.results.length > 0) {
+          const randomIndex = Math.floor(Math.random() * res.results.length);
+          setMovie(res.results[randomIndex]);
+        }
+      })
+      .catch((err) => console.error(err));
+  }, []);
+
+  if (!movie) {
+    return <p>Loading...</p>;
+  }
+  //WHERE IS MY RETURN
   return (
     <div className="text-white relative">
       <img
-        src={HeroBg}
+        src={`https://image.tmdb.org/t/p/original/${
+          movie?.backdrop_path || movie?.poster_path || HeroBg
+        }`}
         className="w-full rounded-2xl h-[520px] object-center object-cover"
         alt="hero bg-img"
       />
