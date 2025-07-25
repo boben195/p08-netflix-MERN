1a20dee8e4fd5ed35c3f9e6d522b4bc089cd1083
boben195 <boben195@ukr.net>
2025-07-25 17:50:02 +0300

    add fetch-user

--- backend/server.js
+++ backend/server.js
@@ -98,6 +98,29 @@ app.post("/api/login", async (req, res) => {
 })
 //************************************************ */
 
+app.get("/api/fetch-user", async (req, res) => {
+  try {
+    const token = req.cookies.token;
+    if (!token) {
+      return res.status(401).json({ message: 'Unauthorized' });
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    if (!decoded || !decoded.id) {
+      return res.status(401).json({ message: 'Unauthorized' });
+    }
+    const user = await User.findById(decoded.id).select('-password');
+    
+    if (!user) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
 
 app.listen(PORT, () => {
   connectDB();  
