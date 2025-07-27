a10ec039e67b049bcc6427645752e455d3f85485
boben195 <boben195@ukr.net>
2025-07-27 17:38:11 +0300

    save user for front

--- backend/server.js
+++ backend/server.js
@@ -123,7 +123,7 @@ app.get("/api/fetch-user", async (req, res) => {
       return res.status(404).json({ message: "User not found" });
     }
 
-    res.status(200).json(user);
+    res.status(200).json({user});
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
